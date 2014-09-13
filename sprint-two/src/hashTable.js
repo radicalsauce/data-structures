079f03430872b4e0ca54f73adece2e7f0479416e
radicalsauce <krhale@gmail.com>
2014-09-12 21:32:32 -0700

    ramblings of madfolk

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,5 +1,6 @@
 var HashTable = function(){
   this._limit = 8;
+  this._size = 0;
   this._storage = makeLimitedArray(this._limit);
 };
 
@@ -8,7 +9,14 @@ HashTable.prototype.insert = function(k, v){
   if(!this._storage.get(i)){
     this._storage.set(i, {});
   }
+  if(!this._storage.get(i)[k]) {
+    this._size++;
+  }
   this._storage.get(i)[k] = v;
+  console.log('insert: ' + k + ', ' + this._size);
+  if(this._size / this._limit > .9){
+    this.double();
+  }
 };
 
 HashTable.prototype.retrieve = function(k){
@@ -18,9 +26,41 @@ HashTable.prototype.retrieve = function(k){
 
 HashTable.prototype.remove = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  if(this._storage.get(i)[k]) {
+    this._size--;
+  }
   this._storage.get(i)[k] = null;
+  console.log('remove: ' + k + ', ' + this._size);
+  if(this._size / this._limit < 0.15){
+    this.halve();
+  }
 };
 
+HashTable.prototype.double = function(){
+  console.log('DOUBLE');
+  this._limit = this._limit * 2;
+  this._size = 0;
+  this._oldStorage = this._storage;
+  this._storage = makeLimitedArray(this._limit);
+  this._oldStorage.each((function(obj){
+    for(var key in obj){
+      this.insert(key, obj[key]);
+    }
+  }).bind(this));
+};
+
+HashTable.prototype.halve = function(){
+  console.log('HALVE');
+  this._limit = this._limit / 2;
+  this._size = 0;
+  this._oldStorage = this._storage;
+  this._storage = makeLimitedArray(this._limit);
+  this._oldStorage.each((function(obj){
+    for(var key in obj){
+      this.insert(key, obj[key]);
+    }
+  }).bind(this));
+};
 
 
 /*
